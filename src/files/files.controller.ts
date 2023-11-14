6c0b5a32084543cc5ce5e93ac1200f45e55d0408
Ryanwalker277 <andianurag277@gmail.com>
2023-11-15 01:31:26 +0530

    Feat: Added support for minio upload and rewrote the file upload service

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,50 +1,65 @@
+import { FilesService } from './files.service';
 import {
   Controller,
-  Get,
-  Param,
   Post,
-  Response,
   UploadedFile,
-  UseGuards,
   UseInterceptors,
+  Param,
+  Get,
+  Res,
+  Body,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
-import { AuthGuard } from '@nestjs/passport';
-import { FilesService } from './files.service';
+import { Express } from 'express';
+import { Response } from 'express';
 
-@ApiTags('Files')
-@Controller({
-  path: 'files',
-  version: '1',
-})
+@Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
-  @ApiBearerAuth()
-  @UseGuards(AuthGuard('jwt'))
   @Post('upload')
-  @ApiConsumes('multipart/form-data')
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        file: {
-          type: 'string',
-          format: 'binary',
-        },
-      },
-    },
-  })
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(
-    @UploadedFile() file: Express.Multer.File | Express.MulterS3.File,
-  ) {
-    return this.filesService.uploadFile(file);
+    @UploadedFile() file: Express.Multer.File,
+    @Param('destination') destination: string,
+    @Body('filename') filename: string,
+  ): Promise<{
+    statusCode: number;
+    message: string;
+    file?: { url: string } | undefined;
+  }> {
+    try {
+      const directory = await this.filesService.upload(file, filename);
+      return {
+        statusCode: 200,
+        message: 'File uploaded successfully',
+        file: { url: directory },
+      };
+    } catch (error) {
+      console.error(`Error uploading file: ${error.message}`);
+      return {
+        statusCode: 500,
+        message: 'File upload failed',
+        file: undefined,
+      };
+    }
   }
 
-  @Get(':path')
-  download(@Param('path') path, @Response() response) {
-    return response.sendFile(path, { root: './files' });
+  @Get('download/:destination')
+  async downloadFile(
+    @Param('destination') destination: string,
+    @Res() res: Response,
+  ): Promise<void> {
+    try {
+      const fileStream = await this.filesService.download(destination);
+      res.set({
+        'Content-Type': 'application/octet-stream',
+        'Content-Disposition': `attachment; filename=${destination}`,
+      });
+      fileStream.pipe(res);
+    } catch (error) {
+      console.error(`Error downloading file: ${error.message}`);
+      res.status(500).send('File download failed');
+    }
   }
 }
