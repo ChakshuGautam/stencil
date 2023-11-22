795224e1d85d438dc187b7039dd640081f61ca76
Yash Mittal <67280631+techsavvyash@users.noreply.github.com>
2023-11-23 02:50:17 +0530

    docs: add docs for monitoring (#46)

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -5,18 +5,29 @@ module.exports = {
       label: 'Getting Started',
       collapsed: false,
       items: ['engineering'],
-    },  
+    },
     {
       type: 'category',
       label: 'Devops',
       collapsed: false,
-      items: ['devops/deployment','devops/containerized_development','devops/github_actions','devops/compose_explaination'],
+      items: [
+        'devops/deployment',
+        'devops/containerized_development',
+        'devops/github_actions',
+        'devops/compose_explaination',
+      ],
     },
     {
       type: 'category',
       label: 'Logging',
       collapsed: false,
-      items: ['logging/overview','logging/elk_setup', 'logging/custom_logger', 'logging/what_to_capture'],
+      items: ['logging/overview', 'logging/elk_setup', 'logging/custom_logger', 'logging/what_to_capture'],
     },
-  ]
+    {
+      type: 'category',
+      label: 'Monitoring',
+      collapsed: false,
+      items: ['monitoring/nestjs-monitor', 'monitoring/setup'],
+    },
+  ],
 };
