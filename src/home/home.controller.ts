d4a269c191e6a478ae4bab54b949de6fa48861b1
Yash Mittal <mittal.yash12@hotmail.com>
2023-12-01 12:20:05 +0530

    feat: integrate the nestJS monitor package

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { HomeService } from './home.service';
+import { ResponseTimeInterceptor } from '@techsavvyash/nestjs-monitor';
 
 @ApiTags('Home')
 @Controller()
 export class HomeController {
   constructor(private service: HomeService) {}
 
-  @Get()
+  @UseInterceptors(
+    new ResponseTimeInterceptor(
+      'home',
+      'monitor/grafana/provisioning/dashboards/response_times.json',
+    ),
+  )
+  @Get('/someroute')
   appInfo() {
     return this.service.appInfo();
   }
